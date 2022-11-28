6df19482f3d49927a6025eaa5ce9cceba8fa1b59
EmaRCB <A17003636@alumnos.uady.mx>
2022-11-27 18:44:25 -0600

    cambios al js

---
+++ SitioWeb/usuario.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuario</title>
+    <link rel="stylesheet" href="./estilos/comun.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Signika:wght@500&display=swap" rel="stylesheet">
+    <script src="plantilla.js"></script>
+</head>
+<body>
+    <div class="grid-container">
+        <div class="item1">
+            <img src="../Recursos/logo.png" alt="logo" class="logo">
+        </div>
+        <div class="item2">
+                <div id="musica" onmouseover="changeColor('var(--music_red)')">
+                    <a href="musica.html" class="banner_text"><strong>M&#218;SICA</strong></a></div>
+                <div id="pintura" onmouseover="changeColor('var(--paint_green)')">
+                    <a href="pintura.html" class="banner_text"><strong>PINTURA</strong></a></div>
+                <div id="artesanias" onmouseover="changeColor('var(--crafts_pink)')">
+                    <a href="artesanias.html" class="banner_text"><strong>ARTESAN&#205;AS</strong></a></div>
+                <div id="baile" onmouseover="changeColor('var(--dance_blue)')">
+                    <a href="baile.html" class="banner_text"><strong>BAILE</strong></a></div>
+                <div id="anunciate" onmouseover="changeColor('var(--advertisement_yellow)')">
+                    <a href="anunciate.html" class="banner_text"><strong>AN&#218;NCIATE</strong></a></div>
+                <div id="eventos-proximos" onmouseover="changeColor('var(--events_purple)')">
+                    <a href="eventos.html" class="banner_text"><strong>EVENTOS</strong></a></div>
+        </div>
+        <div class="item3">
+            <div id="bar"></div>
+        </div>
+        <div class="item4"> 
+            <h1>USUARIO</h1>
+        </div>
+        <div class="item5">
+            <p id="footer_sm">REDES SOCIALES PROXIMAMENTE</p>
+        </div>
+    </div>
+</body>
+</html>
