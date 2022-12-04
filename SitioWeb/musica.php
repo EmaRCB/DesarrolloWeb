9b247707c56ef1ad7288ec3b147f75c6b0bbc018
EmaRCB <A17003636@alumnos.uady.mx>
2022-12-03 23:14:28 -0600

    validacion de usuarios finalizada

--- SitioWeb/musica.php
+++ SitioWeb/musica.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,42 +6,73 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MÚSICA</title>
     <link rel="stylesheet" href="estilos/comun.css">
     <link rel="stylesheet" href="estilos/maquetado2.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Signika:wght@500&display=swap" rel="stylesheet">
     <script>
+        
         function changeColor(color) {
             document.getElementById("bar").style.backgroundColor = color;
         }
+
+        
+        function getImagesArray(){
+            var obj = <?php echo json_encode(imagesArray()); ?>;
+            <?php
+                function imagesArray() {
+                    require "funciones.php";
+                    
+                    $servidor = "localhost: 3307";
+                    $usuario = "emanuel";
+                    $contrasena = "emanuel2002";
+                    $basedatos = "nido_local";
+                    
+                    $sentenciaSQL = "SELECT * FROM proyectos WHERE categoria = 'musica'";
+    
+                    $i = 1;
+    
+    
+                    $resultado = ConsultarSQL($servidor, $usuario, $contrasena, $basedatos, $sentenciaSQL);
+                   
+    
+                    return $resultado;
+                }
+                
+            
+            ?>
+            return obj;
+
+        }
+        
     </script>
 </head>
 
 <body>
     <div class="grid-container">
         <div class="item1">
-            <img src="imagenes/logo.png" alt="logo" class="logo">
+            <a href="./index.php"><img src="../Recursos/logo.png" class="logo" alt="logo"></a>
         </div>
         <div class="item2">
             <div id="musica" onmouseover="changeColor('var(--music_red)')" onmouseout="changeColor('var(--music_red)')">
-                <a href="musica.html" class="banner_text"><strong>M&#218;SICA</strong></a>
+                <a href="musica.php" class="banner_text"><strong>M&#218;SICA</strong></a>
             </div>
             <div id="pintura" onmouseover="changeColor('var(--paint_green)')" onmouseout="changeColor('var(--music_red)')">
-                <a href="pintura.html" class="banner_text"><strong>PINTURA</strong></a>
+                <a href="pintura.php" class="banner_text"><strong>PINTURA</strong></a>
             </div>
             <div id="artesanias" onmouseover="changeColor('var(--crafts_pink)')" onmouseout="changeColor('var(--music_red)')">
-                <a href="artesanias.html" class="banner_text"><strong>ARTESAN&#205;AS</strong></a>
+                <a href="artesanias.php" class="banner_text"><strong>ARTESAN&#205;AS</strong></a>
             </div>
             <div id="baile" onmouseover="changeColor('var(--dance_blue)')" onmouseout="changeColor('var(--music_red)')">
-                <a href="baile.html" class="banner_text"><strong>BAILE</strong></a>
+                <a href="baile.php" class="banner_text"><strong>BAILE</strong></a>
             </div>
             <div id="anunciate" onmouseover="changeColor('var(--advertisement_yellow)')" onmouseout="changeColor('var(--music_red)')">
                 <a href="usuario.php" class="banner_text"><strong>AN&#218;NCIATE</strong></a>
             </div>
             <div id="eventos-proximos" onmouseover="changeColor('var(--events_purple)')" onmouseout="changeColor('var(--music_red)')">
-                <a href="eventos.html" class="banner_text"><strong>EVENTOS</strong></a>
+                <a href="eventos.php" class="banner_text"><strong>EVENTOS</strong></a>
             </div>
         </div>
         <div class="item3">
@@ -190,21 +222,60 @@
 
 
             <div class="imagenesFijas">
-                <div class="grid-item">
-                    <img src="imagenes/arte1.jpeg">
-                </div>
-                <div class="grid-item">
-                    <img src="imagenes/arte2.jpeg">
-                </div>
-                <div id="visibility" class="grid-item">
-                    <img src="imagenes/arte3.jpeg">
-                </div>
-                <div id="visibility" class="grid-item">
-                    <img src="imagenes/arte1.jpeg">
-                </div>
+            <script>
+                window.onload = function(){
+                    const array = getImagesArray();
+                    var arrayLength = array.length ;
+                    for (var i = 0; i < arrayLength; i++) {
+                        addDiv(i);
+                    }
+                }
+                function addDiv(i){
+                    var array = <?php echo json_encode(getImagess()); ?>;
+            <?php
+                function getImagess() {
+                    
+                    $servidor = "localhost: 3307";
+                    $usuario = "emanuel";
+                    $contrasena = "emanuel2002";
+                    $basedatos = "nido_local";
+                    
+                    $sentenciaSQL = "SELECT * FROM proyectos WHERE categoria = 'musica'";
+    
+                    $i = 1;
+    
+    
+                    $resultado = ConsultarSQL($servidor, $usuario, $contrasena, $basedatos, $sentenciaSQL);
+                   
+    
+                    return $resultado;
+                }
+                
+            
+            ?>
+
+                    const g = document.createElement('div');
+                    var name = "imgs-" + i;
+                    g.setAttribute("id", name);
+                    const text = document.createTextNode("");
+                    g.appendChild(text);
+                    
+                    document.getElementsByClassName('imagenesFijas')[0].appendChild(g);
+                    
+                    document.getElementById(name).style.height = "200px";
+                    document.getElementById(name).style.width = "300px";
+                    document.getElementById(name).style.backgroundColor = "white";
+                    document.getElementById(name).style.margin = "15px";
+                    document.getElementById(name).style.backgroundImage = "url('"+array[i]["linkFoto"]+"')";
+                    document.getElementById(name).style.backgroundSize = "cover";
+                    
+                }
+                
+            </script>
+            
             </div>
 
-
+                
         </div>
         <div class="item5">
             <p id="footer_sm">REDES SOCIALES PROXIMAMENTE
